c18d9a271e8c028bd2073d72415e3eae521c2550
Keating <keating.dev@protonmail.com>
2021-11-17 21:56:21 -0500

    Cleanup

--- bot/commands.py
+++ bot/commands.py
@@ -10,7 +10,7 @@ async def malone(ctx):
         await ctx.send(f"Subcommand {ctx.subcommand_passed} is not a valid subcommand!")
 
 
-@malone.command(help="A")
+@malone.command(help="Send users link to report engagement")
 async def report(ctx):
     airtableLinks = read_file()
     airtableLink = airtableLinks.get(str(ctx.guild.id))
@@ -18,4 +18,4 @@ async def report(ctx):
         user = await bot.fetch_user(int(ctx.author.id))
         await user.send(f"Add contributions to the following airtable {airtableLink}")
     else:
-        await ctx.send(f"No airtable link was provided for this Discord server")
+        await ctx.send("No airtable link was provided for this Discord server")
