d569c829cbede86dfc810bd85f8d857ac075378f
Holly Grimm <holly.grimm@gmail.com>
2021-11-27 10:41:51 -0700

    check guild not null

--- bot/commands.py
+++ bot/commands.py
@@ -1,23 +1,24 @@
 import logging
 import discord
-from discord.ext import commands
 from config import read_file, GUILD_IDS
-import constants
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all()
 bot = discord.Bot(intents=intents)
+
 
 @bot.slash_command(guild_id=GUILD_IDS, help="Send users link to report engagement")
 async def report(ctx):
+    if not ctx.guild:
+        await ctx.respond("Please run this command in a guild!")
+        return
+
     airtableLinks = read_file()
     airtableLink = airtableLinks.get(str(ctx.guild.id))
-    if not ctx.guild.id:
-        await ctx.respond("Please run command in a guild!")
-    elif airtableLink:
+
+    if airtableLink:
         await bot.fetch_user(int(ctx.author.id))
         await ctx.send(f"Add contributions to the following airtable {airtableLink}")
     else:
