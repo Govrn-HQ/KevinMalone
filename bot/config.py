acaf3f5126a803afa737ef82e85bd5b8e0663897
Alex Keating <keating.dev@protonmail.com>
2021-12-09 15:45:23 -0500

    Fix build

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,9 @@
 import json
+import os
 
 GUILD_IDS = [747131845317230695, 799328534988193793]
+AIRTABLE_KEY = os.getenv("AIRTABLE_KEY")
+AIRTABLE_BASE = os.getenv("AIRTABLE_BASE")
 
 
 def read_file():
