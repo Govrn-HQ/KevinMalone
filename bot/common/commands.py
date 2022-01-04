4dd8b04ef9326c3aebf052d27bbdd955b0b53319
Alex Keating <keating.dev@protonmail.com>
2022-01-04 18:53:34 -0500

    Fix DM

--- bot/common/commands.py
+++ bot/common/commands.py
@@ -48,8 +48,7 @@ def get_thread(user_id, key):
 
 
 @bot.slash_command(
-    guild_id=GUILD_IDS,
-    description="Send users link to report engagement",
+    guild_id=GUILD_IDS, description="Send users link to report engagement",
 )
 async def report(ctx):
     is_guild = bool(ctx.guild)
@@ -88,9 +87,7 @@ async def join(ctx):
         # on by sending all the commands
         application_commands = bot.application_commands
         embed = discord.Embed(
-            colour=INFO_EMBED_COLOR,
-            title="Welcome Back",
-            description="",
+            colour=INFO_EMBED_COLOR, title="Welcome Back", description="",
         )
         for cmd in application_commands:
             if isinstance(cmd, discord.SlashCommand):
@@ -102,16 +99,7 @@ async def join(ctx):
         return
 
     await ctx.response.defer()
-    # store guild_id and disord_id
-    # check if user can be DMed
-    can_send_message = ctx.can_send(discord.Message)
-    if not can_send_message:
-        await ctx.followup.send(
-            "I cannot onboard you. Please turn on DM's from this server!"
-        )
-        return
 
-    await create_user(ctx.author.id, ctx.guild.id)
     embed = discord.Embed(
         colour=INFO_EMBED_COLOR,
         title="Welcome",
@@ -126,7 +114,16 @@ async def join(ctx):
     logger.info(
         f"Key: {build_cache_value(ThreadKeys.ONBOARDING.value, '', ctx.guild.id)}"
     )
-    message = await ctx.author.send(embed=embed)
+    try:
+
+        message = await ctx.author.send(embed=embed)
+    except discord.Forbidden:
+        message = await ctx.followup.send(
+            "Please enable DM's in order to use the Govrn Bot!", ephemeral=True
+        )
+        return
+
+    await create_user(ctx.author.id, ctx.guild.id)
     await Onboarding(
         ctx.author.id,
         hashlib.sha256("".encode()).hexdigest(),
