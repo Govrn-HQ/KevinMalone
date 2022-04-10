8bc6d3649b2b23270f4207b0aa232e51375ebeb3
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-04-09 21:32:58 -0400

    add consts for wallet and twitter verification to config

--- bot/config.py
+++ bot/config.py
@@ -8,6 +8,23 @@ REDIS_URL = constants.Bot.redis_url
 AIRTABLE_KEY = constants.Bot.airtable_key
 AIRTABLE_BASE = constants.Bot.airtable_base
 
+REQUESTED_TWEET = "Kevin Malone told me to say this"
+TWITTER_URL_REGEXP = r"^https://twitter.com/(.+)/status/([0-9]+)"
+MAX_TWEETS_TO_RETRIEVE = 5
+MAX_TWEET_LOOKBACK_MINUTES = 10
+
+REQUESTED_SIGNED_MESSAGE = "Kevin Malone told me to sign this"
+WALLET_VERIFICATION_INSTRUCTIONS_FMT = (
+    "To verify you have access to wallet at address %s, follow these steps:"
+    "\n1: Navigate to myetherwallet.com/wallet/sign"
+    "\n2: When prompted, connect with your metamask wallet in the browser"
+    "\n3: Copy and paste the message below into the 'Signature' box"
+    "\n4: Click the 'Sign' button"
+    "\n5: Approve the metamask notification and sign the message"
+    "\n6: Copy the long string right after 'sig' (without quotes)"
+    "\n7: Paste that string into this conversation to verify ownership of %s"
+)
+
 YES_EMOJI = "\U0001F44D"
 NO_EMOJI = "\U0001F44E"
 SKIP_EMOJI = "\U000023ED"
