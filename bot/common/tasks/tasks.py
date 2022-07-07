71da6af169950542eac844bb02aa603bcaba74af
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-07-07 13:53:53 -0400

    complete weekly cadence logic, black fmt

--- bot/common/tasks/tasks.py
+++ bot/common/tasks/tasks.py
@@ -2,14 +2,15 @@ import threading
 import logging
 import asyncio
 
-from bot.common.cache import Cache, RedisCache
+from bot.common.cache import Cache
 from abc import ABC, abstractmethod
-from datetime import timedelta, datetime, time
+from datetime import timedelta, datetime, time, date
 
 logger = logging.getLogger(__name__)
 
 TASK_LOOP_INTERVAL_MINUTES = 10
 REPORT_LAST_SENT_DATETIME_CACHE_KEY = "contribution_report_last_sent"
+DATETIME_CACHE_FMT = "%m/%d/%Y, %H:%M:%S"
 
 
 class Task:
@@ -59,18 +60,20 @@ class Cadence(ABC):
     async def update_last_run_in_cache(self):
         await self.cache.set(
             REPORT_LAST_SENT_DATETIME_CACHE_KEY,
-            datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+            datetime.now().strftime(DATETIME_CACHE_FMT),
         )
 
 
 class Weekly(Cadence):
-    def __init__(self, cache: Cache, cache_key: str, day_to_run: int, time_to_run: time):
+    def __init__(
+        self, cache: Cache, cache_key: str, day_to_run: int, time_to_run: time
+    ):
         self.cache = cache
         self.cache_key = cache_key
         self.day_to_run = day_to_run
         self.time_to_run = time_to_run
 
-    # last_sent should be used to make sure that the bot doesn't 
+    # last_sent should be used to make sure that the bot doesn't
     # always send something on a restart
     async def timedelta_until_run(self) -> timedelta:
         last_sent = await self.cache.get(self.cache_key)
@@ -78,25 +81,47 @@ class Weekly(Cadence):
         # the last update was sent 1 week ago
         now: datetime = datetime.now()
 
-        # last_entry does not exist -> return delta to closest ocurrence of day to run, time
+        # last_entry does not exist -> return delta to closest ocurrence of next run
         if last_sent is None:
-            if now.weekday() == self.day_to_run:
+            return self.get_timedelta(now)
+
+        last_sent: datetime = datetime.strptime(last_sent)
+
+        # if the last_sent is on a day that does not match specified weekday,
+        # the cadence may have been changed in code, or the task ran late
+        # for some reason. log appropriately, return timedelta for next ocurrence
+        if last_sent.weekday() != self.day_to_run:
+            logger.warning(
+                f"Weekly task {self.cache_key} was last run on a different day "
+                f"({last_sent.strftime(DATETIME_CACHE_FMT)}) than is specified on "
+                f"the task ({self.time_to_run.isoformat()} on {self.day_to_run}th "
+                "day of the week)"
+            )
+            return self.get_timedelta(now)
+
+        return self.get_timedelta_with_last_sent(now, last_sent)
+
+    def get_timedelta(self, now: datetime) -> timedelta:
+        if now.weekday() == self.day_to_run:
+            if now.time >= self.time_to_run:
                 # run immediately
                 return timedelta(seconds=-1)
-            # return the timedelta for the next occurrence of the day and time to run
+        # wait until the next ocurrence of the specified date and time
+        return self.get_timedelta_for_next_occurrence(now)
 
-        # case 1: last_entry exists -> return delta to next ocurrence of day to run, time to run
-        # do we need handling of last_entries on odd days?
+    def get_timedelta_for_next_occurrence(self, now: datetime) -> timedelta:
+        days_until = (self.day_to_run - now.weekday()) % 7
+        next_ocurrence = now + timedelta(days=days_until)
+        next_ocurrence = datetime.combine(next_ocurrence.date, self.time_to_run)
+        return next_ocurrence - now
 
-        # if there is no last_sent entry in the cache, then wait for the
-        # next ocurrence of the day + time to run
-
-        td: timedelta = (
-            None if last_sent is None else now - datetime.strptime(last_sent)
-        )
-        if td.days >= 7 and now.date().weekday() == self.day_to_run:
-            return True
-        return False
+    def get_timedelta_with_last_sent(
+        self, now: datetime, last_sent: datetime
+    ) -> timedelta:
+        # If the last_sent datetime is today, wait until next ocurrence
+        if now.date == last_sent.date:
+            return self.get_timedelta_for_next_occurrence(now)
+        return self.get_timedelta(now)
 
 
 # conforms with date.weekday()
