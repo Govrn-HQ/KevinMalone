34edec8d4da42ac4c070ddb802c3fd925bc3ce83
Flip Liquid <13227294+Flip-Liquid@users.noreply.github.com>
2022-07-26 15:39:35 -0400

    Revert "Revert "Staging""
    This reverts commit a4e8045531a389e71901e3371112ebb0ac06887c.

--- bot/common/commands.py
+++ bot/common/commands.py
@@ -1,5 +1,4 @@
 from bot import constants
-from discord import option
 from discord.commands import Option
 from distutils.util import strtobool
 import logging
@@ -9,29 +8,29 @@ import discord
 
 from bot.common.airtable import (
     find_user,
-    fetch_user,
-    create_user,
     get_guild,
 )
 from bot.common.bot.bot import bot
+from bot.common.graphql import get_guild_by_discord_id
 from bot.common.threads.thread_builder import (
     build_cache_value,
     ThreadKeys,
 )
 from bot.common.threads.onboarding import Onboarding
 from bot.common.threads.report import ReportStep, get_reporting_link
-from bot.common.threads.points import Points
 from bot.common.threads.update import UpdateProfile
 from bot.common.threads.add_dao import AddDao
+from bot.common.threads.history import History
+from bot.common.threads.guild_select import GuildSelect
+from bot.common.threads.utils import get_thread
 from bot.config import (
     GUILD_IDS,
     INFO_EMBED_COLOR,
     Redis,
     get_list_of_emojis,
 )
+from discord import errors
 from bot.exceptions import NotGuildException, ErrorHandler
-from bot.common.guild_select import get_thread, GuildSelect
-from web3 import Web3
 
 
 logger = logging.getLogger(__name__)
@@ -91,20 +90,22 @@ async def report(ctx):
 
 
 @bot.slash_command(guild_id=GUILD_IDS, description="Get started with Govrn")
-@option(
-    "wallet", description="Enter your ethereum wallet address (No ENS)", required=True
-)
-async def join(ctx, wallet):
+async def join(ctx):
     is_guild = bool(ctx.guild)
     if not is_guild:
         raise NotGuildException("Command was executed outside of a guild")
-    if not Web3.isAddress(wallet):
-        await ctx.response.send_message("Not a valid wallet address", ephemeral=True)
-        ctx.response.is_done()
-        return
 
-    is_user = await find_user(ctx.author.id)
-    if is_user:
+    # defer response before awaiting queries to db
+    await ctx.response.defer()
+
+    guild_discord_id = ctx.guild.id
+
+    # TODO: a single query could be written for this
+    user = await find_user(ctx.author.id)
+    guild = await get_guild_by_discord_id(guild_discord_id)
+    guild_id = guild["id"]
+
+    if any(guild_user["guild_id"] == guild_id for guild_user in user["guild_users"]):
         # Send welcome message and
         # And ask what journey they are
         # on by sending all the commands
@@ -124,8 +125,6 @@ async def join(ctx, wallet):
         ctx.response.is_done()
         return
 
-    await ctx.response.defer()
-
     embed = discord.Embed(
         colour=INFO_EMBED_COLOR,
         title="Welcome",
@@ -140,8 +139,8 @@ async def join(ctx, wallet):
     logger.info(
         f"Key: {build_cache_value(ThreadKeys.ONBOARDING.value, '', ctx.guild.id)}"
     )
-    try:
 
+    try:
         message = await ctx.author.send(embed=embed)
     except discord.Forbidden:
         message = await ctx.followup.send(
@@ -149,22 +148,26 @@ async def join(ctx, wallet):
         )
         return
 
-    # Check if user exists
-    #
-    # If user does not exist ask for wallet address
-    # then create
-    user = await fetch_user(ctx.author.id)
-    print(user)
-    if not user:
-        await create_user(ctx.author.id, ctx.guild.id, wallet)
-    onboarding = await Onboarding(
+    await ctx.followup.send("Check your DM's to continue onboarding", ephemeral=True)
+
+    thread = await Onboarding(
         ctx.author.id,
         hashlib.sha256("".encode()).hexdigest(),
         message.id,
         ctx.guild.id,
     )
-    await onboarding.send(message)
-    await ctx.followup.send("Check your DM's to continue onboarding", ephemeral=True)
+    # Need to set the metadata here to provide the guild id
+    await Redis.set(
+        ctx.author.id,
+        build_cache_value(
+            thread=ThreadKeys.ONBOARDING.value,
+            step=thread.steps.hash_,
+            guild_id=ctx.guild.id,
+            message_id=message.id,
+            metadata={"guild_name": ctx.guild.name},
+        ),
+    )
+    await thread.send(message)
 
 
 @bot.slash_command(
@@ -211,9 +214,9 @@ async def update(ctx):
 
 @bot.slash_command(
     guild_id=GUILD_IDS,
-    description="Send user points for a given community",
+    description="Send user history for a given community",
 )
-async def points(
+async def history(
     ctx,
     days: Option(
         str,
@@ -231,7 +234,7 @@ async def points(
         error_embed = discord.Embed(
             colour=INFO_EMBED_COLOR,
             description="You are not a part of any communities. "
-            "Please run the /points command in a guild you are in",
+            "Please run the /history command in a guild you are in",
         )
 
         message, metadata = await select_guild(ctx, embed, error_embed)
@@ -259,7 +262,7 @@ async def points(
             ),
         )
 
-    thread = await Points(
+    thread = await History(
         ctx.author.id,
         hashlib.sha256("".encode()).hexdigest(),
         None,
@@ -371,25 +374,22 @@ if bool(strtobool(constants.Bot.is_dev)):
 
 
 async def select_guild(ctx, response_embed, error_embed):
+    await ctx.response.defer()
     discord_rec = await find_user(ctx.author.id)
-    airtable_guild_ids = discord_rec.get("guild_users")
-    if not airtable_guild_ids:
+    guild_ids = discord_rec.get("guild_users")
+    if not guild_ids:
         await ctx.response.send_message(embed=error_embed)
         ctx.response.is_done()
         return None, None
 
-    await ctx.response.defer()
     guild_metadata = []
-    # TODO: this will set Thread.guild_id to the database id of the guild
-    # there are many places which still use guild_id to represent the discord
-    # id of the guild itself. We should standardize on guild_id vs guild_discord_id
-    # everywhere that's appropriate. Also with user_id vs user_discord_id
-    for record_id in airtable_guild_ids:
+    for record_id in guild_ids:
         g = await get_guild(record_id.get("guild_id"))
         guild_id = g.get("id")
+        guild_discord_id = g.get("discord_id")
         guild_name = g.get("name")
         if guild_id:
-            guild_metadata.append([guild_id, guild_name])
+            guild_metadata.append([guild_id, guild_name, guild_discord_id])
     embed = response_embed
     emojis = get_list_of_emojis(len(guild_metadata))
     daos = {}
@@ -398,7 +398,7 @@ async def select_guild(ctx, response_embed, error_embed):
         # hasn't yet been added to the guild_id in question
         # guild = await bot.fetch_guild(guild_id)
         emoji = emojis[idx]
-        daos[emoji] = guild_data[0]
+        daos[emoji] = {"guild_id": guild_data[0], "guild_discord_id": guild_data[2]}
         embed.add_field(name=guild_data[1], value=emoji)
     message = await ctx.followup.send(embed=embed)
     for emoji in emojis:
@@ -431,7 +431,11 @@ async def on_message(message):
         return
 
     thread = await get_thread(message.author.id, thread_key)
-    await thread.send(message)
+
+    try:
+        await thread.send(message)
+    except errors.ApplicationCommandError as e:
+        await message.channel.send(str(e))
 
 
 @bot.event
--- bot/common/threads/add_dao.py
+++ bot/common/threads/add_dao.py
@@ -7,19 +7,21 @@ from bot.common.threads.thread_builder import (
     StepKeys,
     Step,
 )
-from bot.common.airtable import (
-    get_guild_by_guild_id,
+from bot.common.graphql import (
+    fetch_user_by_discord_id,
+    get_guild_by_discord_id,
     create_guild,
-    update_guild,
-    find_user,
+    update_guild_name,
 )
-from bot.common.graphql import create_guild_user
 from bot.common.threads.thread_builder import (
     write_cache_metadata,
     get_cache_metadata_key,
 )
 from bot.exceptions import ThreadTerminatingException
 
+# from bot.common.threads.utils import (  # noqa: E402
+# set_thread_and_send
+# )
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +29,7 @@ logger = logging.getLogger(__name__)
 class AddDaoPromptId(BaseStep):
     """Prompts the user to input the discord ID of the guild they wish to add"""
 
-    name = StepKeys.ADD_DAO_PROMPT_ID
+    name = StepKeys.ADD_DAO_PROMPT_ID.value
 
     def __init__(self, cache):
         super().__init__()
@@ -42,7 +44,19 @@ class AddDaoPromptId(BaseStep):
         )
         return sent_message, None
 
-    async def save(self, message, guild_id, user_id):
+
+class AddDaoGetOrCreate(BaseStep):
+    """This step checks if the supplied DAO ID already exists, and controls
+    the flow accordingly to either create the DAO + user, or direct the
+    user down the join flow."""
+
+    name = StepKeys.ADD_DAO_GET_OR_CREATE.value
+
+    def __init__(self, parent_thread, cache):
+        self.parent_thread = parent_thread
+        self.cache = cache
+
+    async def send(self, message, user_id):
         message_content = message.content.strip()
         dao_id = None
 
@@ -55,28 +69,46 @@ class AddDaoPromptId(BaseStep):
             message = f"{message_content} is not a valid discord id!"
             raise ThreadTerminatingException(message)
 
-        guild = await get_guild_by_guild_id(dao_id)
+        return None, {"guild_id": dao_id}
+
+    async def control_hook(self, message, user_id):
+        dao_id = str(int(message.content.strip()))
+        self.parent_thread.guild_id = dao_id
+        guild = await get_guild_by_discord_id(dao_id)
         if guild:
-            message = (
-                f"It looks like guild {dao_id} has already been onboarded as "
-                f"{guild.get('name')}! You can report your "
-                " contributions with /report!"
-            )
-            raise ThreadTerminatingException(message)
+            # Check if user is a member
+            user = await fetch_user_by_discord_id(user_id)
+            guild_name = guild.get("name")
+
+            if user is not None and any(
+                guild_user.get("guild_id") == guild.get("id")
+                for guild_user in user.get("guild_users")
+            ):
+                message = (
+                    f"It looks like guild {dao_id} has already been added as "
+                    f"{guild_name}, and it looks like you're already a member! "
+                    " You can report your contributions with /report!"
+                )
+                raise ThreadTerminatingException(message)
+
+            # guild exists, user does not, drop into /join flow
+            await write_cache_metadata(user_id, self.cache, "guild_id", dao_id)
+            await write_cache_metadata(user_id, self.cache, "guild_name", guild_name)
+            return StepKeys.ADD_DAO_PREVIOUSLY_ADDED_PROMPT.value
+
+        await create_guild(dao_id)
 
         # add validated dao_id to metadata cache for lookup on next step
         await write_cache_metadata(user_id, self.cache, "guild_id", dao_id)
 
-        user = await find_user(user_id)
-        id = await create_guild(user.get("id"), dao_id)
-        # Create guild user
-        await create_guild_user(user.get("id"), id.get("id"))
+        # Prompt for guild name
+        return StepKeys.ADD_DAO_PROMPT_NAME.value
 
 
 class AddDaoPromptName(BaseStep):
     """Prompts the user to input the name of the guild they wish to add"""
 
-    name = StepKeys.ADD_DAO_PROMPT_NAME
+    name = StepKeys.ADD_DAO_PROMPT_NAME.value
 
     def __init__(self, cache):
         super().__init__()
@@ -90,17 +122,41 @@ class AddDaoPromptName(BaseStep):
         return sent_message, None
 
     async def save(self, message, guild_id, user_id):
-        # retrieve dao_id from cache
         guild_name = message.content.strip()
+        # retrieve dao_id from cache
         dao_id = await get_cache_metadata_key(user_id, self.cache, "guild_id")
-        await update_guild(dao_id, "guild_name", guild_name)
+        await update_guild_name(dao_id, guild_name)
         await write_cache_metadata(user_id, self.cache, "guild_name", guild_name)
+
+
+class AddDaoPreviouslyAddedPrompt(BaseStep):
+    """Prompts that a particular DAO has already been added"""
+
+    name = StepKeys.ADD_DAO_PREVIOUSLY_ADDED_PROMPT.value
+    trigger = True
+
+    def __init__(self, cache):
+        super().__init__()
+        self.cache = cache
+
+    async def send(self, message, user_id):
+        guild_name = await get_cache_metadata_key(user_id, self.cache, "guild_name")
+        guild_id = await get_cache_metadata_key(user_id, self.cache, "guild_id")
+
+        return (
+            await message.channel.send(
+                f"It looks like guild {guild_id} has already been added as "
+                f"{guild_name}, but you're not yet a member. Let's get that set up!"
+            ),
+            None,
+        )
 
 
 class AddDaoSuccess(BaseStep):
     """Sends a success message for adding the Guild"""
 
-    name = StepKeys.ADD_DAO_SUCCESS
+    name = StepKeys.ADD_DAO_SUCCESS.value
+    trigger = True
 
     def __init__(self, cache):
         super().__init__()
@@ -110,10 +166,33 @@ class AddDaoSuccess(BaseStep):
         guild_name = await get_cache_metadata_key(user_id, self.cache, "guild_name")
         return (
             await message.channel.send(
-                f"Thanks for adding {guild_name} as a new guild! You can now "
-                "report your contributions using the /report command."
+                f"Thanks for adding {guild_name} as a new guild! Let's get "
+                "you set up with a profile for this guild. After setting it up, "
+                "you can report your contributions using the /report command."
             ),
             None,
+        )
+
+
+class AddDaoJoinFlowOverride(BaseStep):
+    """Overrides the Add_Dao flow and drops user into join flow"""
+
+    name = StepKeys.ADD_DAO_JOIN.value
+
+    def __init__(self, cache, parent_thread):
+        super().__init__()
+        self.cache = cache
+        self.parent_thread = parent_thread
+
+    # TODO: check reqs for join flow re: cache/populated fields
+    async def send(self, message, user_id):
+        guild_id = await get_cache_metadata_key(user_id, self.cache, "guild_id")
+        return await set_thread_and_send(
+            current_thread=self.parent_thread,
+            next_thread_key=ThreadKeys.ONBOARDING.value,
+            message=message,
+            user_id=user_id,
+            guild_id=guild_id,
         )
 
 
@@ -121,10 +200,23 @@ class AddDao(BaseThread):
     name = ThreadKeys.ADD_DAO.value
 
     async def get_steps(self):
+        dao_not_added_steps = (
+            Step(current=AddDaoPromptName(self.cache))
+            .add_next_step(AddDaoSuccess(self.cache))
+            .add_next_step(AddDaoJoinFlowOverride(cache=self.cache, parent_thread=self))
+        ).build()
+        dao_previously_added_steps = (
+            Step(current=AddDaoPreviouslyAddedPrompt(self.cache)).add_next_step(
+                AddDaoJoinFlowOverride(cache=self.cache, parent_thread=self)
+            )
+        ).build()
         steps = (
             Step(current=AddDaoPromptId(self.cache))
-            .add_next_step(AddDaoPromptName(self.cache))
-            .add_next_step(AddDaoSuccess(self.cache))
+            .add_next_step(AddDaoGetOrCreate(self, self.cache))
+            .fork([dao_not_added_steps, dao_previously_added_steps])
         )
 
         return steps.build()
+
+
+from bot.common.threads.utils import set_thread_and_send  # noqa: E402
